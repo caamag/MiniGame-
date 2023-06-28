245faa7e45d4e3705a7c3d32dbc95f9f5bb0eacf
Caio Magalhães <caiolopesfv@gmail.com>
2023-06-27 22:11:51 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,94 @@
+
+const tela = document.querySelector('.container'); 
+const mario = document.querySelector('.mario'); 
+const tunel = document.querySelector('.tunel'); 
+const chao = document.querySelector('.chao'); 
+const nuvem = document.querySelector('.nuvem'); 
+
+const lose = document.querySelector('.lose'); 
+const pLose = document.querySelector('.pLose'); 
+const scoreFinal = document.querySelector('.scoreFinal'); 
+
+
+
+
+
+
+//acionando animação do pulo:
+
+document.addEventListener('click', () => {
+
+    marioJunp(); 
+
+})
+
+function marioJunp () {
+
+    mario.classList.add('marioPulo'); 
+
+    setTimeout(()  => {
+        mario.classList.remove('marioPulo'); 
+    }, 700); 
+
+}
+
+
+
+
+
+//detectando se o jogador encostou no túnel:
+
+function obstaculo () {
+
+    setInterval(() => {
+    
+        const distanciaTunel = tunel.offsetLeft; 
+        const alturaMario = window.getComputedStyle(mario).bottom.replace('px', ''); 
+
+        if (distanciaTunel <= 155 && alturaMario <= 80) {
+
+            tunel.style.display = 'none'; 
+            chao.style.animation = 'none'; 
+            nuvem.style.display = 'none'; 
+            mario.style.animation = 'none'; 
+
+            mario.src = '../img/game-over.png'; 
+            mario.style.width = '100px'; 
+
+            lose.style.display = 'block'; 
+            pLose.style.display = 'block'; 
+            scoreFinal.style.display = 'block'; 
+            scoreFinal.innerHTML = `Score final: ${scoreInicial}`
+            clearInterval(time); 
+
+
+
+        } else {
+            return; 
+        }
+
+    }, 10)
+
+}
+
+obstaculo(); 
+
+
+
+
+//score:
+const score = document.querySelector('.score'); 
+
+let scoreInicial = 0; 
+let time; 
+
+function contador() {
+
+time = setInterval(function(){
+scoreInicial++; 
+score.innerHTML = `Score: ${scoreInicial}`; 
+}, 100); 
+
+}; 
+
+contador(); 
